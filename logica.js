c84e075c0749b03db868d00ff174ba6987d6283c
Sebastian <jsortiz82@misena.edu.co>
2023-06-16 15:37:16 -0500

    Se crean funciones para calcular perimetro y area de un tringulo, adicionalmente se llama la funcion desde el index.html

--- logica.js
+++ logica.js
@@ -19,3 +19,33 @@ function calcArea(){
     document.getElementById('result').innerText = resultadoArea;
 
 }
+
+function calcPerimetroTriangulo(){
+    let sideT1 = document.getElementById('sideT1');
+    let sideT2 = document.getElementById('sideT2');
+    let sideT3 = document.getElementById('sideT3');
+
+    let valueT1 = parseInt(sideT1.value);
+    let valueT2 = parseInt(sideT2.value);
+    let valueT3 = parseInt(sideT3.value);
+    let PerimetroT = valueT1 + valueT2 + valueT3;
+
+    resultadoPerT = `El perimetro del triangulo es: ${PerimetroT} cm`
+    document.getElementById('result').innerText = resultadoPerT;
+
+}
+
+function calcAreaTriangulo(){
+    let altT =document.getElementById('t-base').value;
+    let baseT = document.getElementById('t-altura').value;
+    altT = parseInt(altT);
+    baseT = parseInt(baseT);
+
+    let areaT = altT * baseT;
+
+    resultadoAt = `El area del triangulo es : ${areaT} cm`;
+    document.getElementById('result').innerText = resultadoAt;
+
+
+}
+
